d65bcec685bc865fea6e4ab45025f491e0c41700
Slamanii <adrianovwigho06@gmail.com>
2026-08-13 20:15:28 +0100

    Step 1

---
+++ server.js
@@ -0,0 +1,10 @@
+import express from 'express'
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Hello Server' })
+});
+app.listen(3002, () => {
+    console.log('Server is running on port 3002')
+})

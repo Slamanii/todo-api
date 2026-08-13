56307f258f3d41801e76be01ddc7d6017343f2c3
Slamanii <adrianovwigho06@gmail.com>
2026-08-13 20:19:12 +0100

    Step 2

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const app = express();
 app.get('/', (req, res) => {
     res.json({ message: 'Hello Server' })
 });
+
+app.get('/health', (req, res) => {
+    res.json({ status: "Ok" })
+})
+
 app.listen(3002, () => {
     console.log('Server is running on port 3002')
 })

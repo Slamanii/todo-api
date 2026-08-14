b39103ca2de9f156185c838e32d5c60980220dde
Slamanii <adrianovwigho06@gmail.com>
2026-08-14 17:07:41 +0100

    Integrated sq-lite3

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const Database = require('better-sqlite3');
+
+const db = new Database('tasks.db');
 
 const app = express();
 
@@ -9,11 +12,37 @@ app.use('/docs', swaggerUi.serve, swaggerUi.setup(openapiSpec));
 
 app.use(express.json());
 
-const tasks = [
+{ /* const tasks = [
     { id: 1, title: 'Learn Node.js', done: true },
     { id: 2, title: 'Complete Todo App using Express', done: false },
     { id: 3, title: 'Test the Api', done: false }
 ];
+*/
+}
+
+db.prepare(` 
+    CREATE TABLE IF NOT EXISTS tasks (
+    id INTEGER PRIMARY KEY,
+    title TEXT NOT NULL,
+    done BOOLEAN NOT NULL DEFAULT 0
+)
+`).run();
+
+const count = db.prepare(`SELECT COUNT(*) AS count FROM tasks`).get();
+
+if (count.count === 0) {
+    const insert = db.prepare(`
+        INSERT INTO tasks (title, done)
+        VALUES (?, ?)
+        `);
+
+    insert.run('Learn Node.js', 1);
+    insert.run('Complete Todo App using Express', 0);
+    insert.run('Test the API', 0);
+}
+
+ 
+
 
 app.get('/', (req, res) => {
     res.json({ message: 'Hello Server' })
@@ -24,19 +53,28 @@ app.get('/health', (req, res) => {
 })
 
 app.get('/tasks', (req, res) => {
-    res.json({ tasks })
+    const tasks = db.prepare(`SELECT * FROM tasks`).all();
+
+    const formattedTasks = tasks.map(task => ({
+        ...task,
+        done: Boolean(task.done)
+    }))
+    res.json(formattedTasks);
 })
 
 app.get('/tasks/:id', (req, res) => {
     const taskId = Number(req.params.id)
 
-    const task = tasks.find(t => t.id === taskId);
+    const task = db.prepare(`SELECT * FROM tasks WHERE id = ?`).get(taskId);
 
     if (!task) {
        return res.status(404).json({ error: "Task not found" });
     }
 
-    res.json(task);
+    res.json({
+        ...task,
+        done: Boolean(task.done)
+    });
 });
 
 app.post('/tasks/', (req, res) => {
